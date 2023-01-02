7d32a4cca482990fda4c6a25b58fbd5ae0a50182
Hiroaki Kobayashi <kobtea9696@gmail.com>
2023-01-03 04:33:52 +0900

    fix git client error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 version=${INPUT_PREFIX}$($INPUT_ONLY_MAJOR_VERSION && (cat VERSION | cut -d. -f1) || cat VERSION)${INPUT_SUFFIX}
 message=$(echo $INPUT_MESSAGE | sed s/__VERSION__/$version/g)
+git config --global --add safe.directory /github/workspace
 git config user.name  || git config --local user.name $(git log -1 --pretty=format:'%an')
 git config user.email || git config --local user.email $(git log -1 --pretty=format:'%ae')
 if $INPUT_FORCE; then
@@ -8,4 +9,4 @@ if $INPUT_FORCE; then
 else
     git ls-remote --exit-code --tags origin $version > /dev/null 2>&1 || (git tag -a $version -m "$message" && git push origin $version)
 fi
-echo "::set-output name=tag::$version"
+echo "tag=$version" >> $GITHUB_OUTPUT
